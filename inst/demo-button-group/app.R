a2dd99fc3a87293445ee74ebb91ab8026dadd4dd
DivadNojnarg <dgranjon@ymail.com>
2021-09-09 20:30:53 +0200

    add 2 demo apps

---
+++ inst/demo-button-group/app.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(reactstrapTest)
+
+ui <- fluidPage(
+  theme = bslib::bs_theme(version = "4"),
+  button_group("plop", "A group of buttons", choices = names(mtcars))
+)
+
+server <- function(input, output, session) {
+  observe(print(input$plop))
+}
+
+shinyApp(ui, server)
